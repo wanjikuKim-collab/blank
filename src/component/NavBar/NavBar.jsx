f9c31d49873dc6cb0c5d76ec0899916a5884383f
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-24 16:08:30 +0300

    created lnks to the pages

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import SecondaryNAvigation from '../SecondaryNavigation/SecondaryNAvigation'
 import styles from "./NavBar.module.css"
+import { NavLink } from 'react-router-dom'
 
-function NavBar() {
-  const navItems = [
-    'Home',
-    'TV Shows',
-    'Movies',
-    'New & Popular',
-    'My List',
-    'Browse by Languages'
-
+function NavBar({isScrolled}) {
+  const links = [
+   { name:'Home', link:"/"},
+   { name:'TV Shows', link:"/series"},
+   { name:'Movies', link:"/movies"},
+   { name:'New & Popular', link:"/new&popular"},
+   { name:'My List', link:"/mylist"},
+   { name:'Browse by Languages', link:"/languages"}
   ]
+
   return (
     <div className={styles.nav}>
       <div className={styles.mainNav}>
         <img className={styles.logo} src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="Netflix Logo"/>      
         <ul className={styles.navItems}>
           {
-            navItems.map(item=><li key={item}>{item}</li>)
-          }
+            links.map((link)=>{
+              return(<li key={link.name}>
+                <NavLink className={styles.Link} to={link.link}>{link.name}</NavLink>
+              </li>)
+            })
+          }       
         </ul>       
       </div>
       <SecondaryNAvigation />
