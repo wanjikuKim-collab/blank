f15deb2d6f1c837c663c79ce5f2f54c151e23536
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-24 16:10:23 +0300

    app pages

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import NavBar from "../component/NavBar/NavBar";
+
+
 
 function HomeScreen() {
+  const [isScrolled, setIsScrolled]= useState(false);
+  
+  window.onscroll=()=>{
+    // user at the top of the screen
+    setIsScrolled(window.pageYOffset === 0 ? false :true);
+    return ()=>(window.onscroll = null)
+  }
+ 
   return (
-    <div>HomeScreen</div>
+    <div>
+      <NavBar isScrolled ={isScrolled}/>
+    </div>
   )
 }
 
---
+++ src/pages/TV_Shows.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TV_Shows() {
+  return (
+    <div>TV_Shows</div>
+  )
+}
+
+export default TV_Shows
