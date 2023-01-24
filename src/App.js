d80a06e5c00259d9c603a2f247d7d323b2e2985e
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-24 16:06:50 +0300

    created page route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
 import NavBar from './component/NavBar/NavBar';
-import BillBoard from './component/BillBoard';
-import Footer from './component/Footer';
-import Row from './component/Row';
+import { Routes, Route } from 'react-router-dom';
+import HomeScreen from './pages/HomeScreen';
+import TV_Shows from './pages/TV_Shows';
+import Movies from './pages/Movies';
+import Latest from './Latest';
+import Languages from './pages/Languages';
+import MyList from './pages/MyList';
 
 function App() {
+
   return (
     <div className="App">
-      <NavBar />
-      <BillBoard />
-      <Row />
-      <Footer/>
+     <Routes>
+      <Route path="/" element={<HomeScreen/>}/> 
+      <Route path="/series" element={<TV_Shows/>}/>
+      <Route path="/movies" element={<Movies/>}/>
+      <Route path="/new&popular" element={<Latest/>}/> 
+      <Route path="/mylist" element={<MyList/>}/>
+      <Route path="/languages" element={<Languages/>}/>
+     </Routes>
     </div>
   );
 }
