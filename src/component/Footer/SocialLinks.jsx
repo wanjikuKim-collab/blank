949c25779fda930a7b47d944a5e892b0a06a7f0b
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-24 18:24:29 +0300

    completed social links

---
+++ src/component/Footer/SocialLinks.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './SocialLinks.module.css'
+
+function SocialLinks() {
+  return (
+    <div className={styles.socials}>
+       <span>
+        <a href=""><i className="big icon facebook" /></a>
+      </span>
+      <span>    
+        <a href=""><i className="big icon instagram" /></a>
+      </span>
+      <span>    
+        <a href=""><i className="big icon youtube" /></a>
+      </span>      
+    </div>
+  )
+}
+
+export default SocialLinks
