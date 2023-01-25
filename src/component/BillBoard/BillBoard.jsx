890efbbe0bc493d1515e16f464b6520149b8d401
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-25 19:38:52 +0300

    added a truncate function to the description

--- src/component/BillBoard/BillBoard.jsx
+++ src/component/BillBoard/BillBoard.jsx
@@ -5,31 +5,43 @@ import billboardHeroTitle from './assets/Title.webp'
 
 
 function BillBoard() {
+
+  const truncate =(string,n)=>{
+    return string?.length > n ? string.substr(0,n-1) + '...': string
+  }
+
   return (
     
     <div className={styles.billboard}>      
       <div className={styles.innerContainer}>
         <div className={styles.imageWrapper}>
+          {/* BILLBOARD IMAGE*/}
           <img className={styles.heroImage} src={billboardHeroImage} alt="Arisu in the middle of Tokyo which has skyscrapers covered in overgrown plants" />
+          <div className={styles.billboard__fadeBottom}/>
         </div>
-        <div className={styles.info} >
-          <img src={billboardHeroTitle} alt="Alice in Boderland" />
+         {/* BILLBOARD DETAILS*/}
+        <div className={styles.info} >           
+          <img src={billboardHeroTitle} alt="Alice in Boderland" /> 
+
           <div className={styles.description}>
-            An aimless gamer and his two friends find themselves in a paralle Tokyo, where they're forced to compete in a series of sadistic games to survive.
+            {
+              truncate(`An aimless gamer and his two friends find themselves in a paralle Tokyo, where they/'re forced to compete in a series of sadistic games to survive.`,150)
+            }
+            
           </div>
-          
-          <div className={styles. buttonLinks}>
-            <a className='btn btn-light btn-lg'> 
+
+          <div className={styles.buttonLinks}>
+            <a className='billboard_btn btn btn-light btn-lg' href='/'> 
             <i className="icon play" />
               <span>Play</span>               
             </a>
-            <button className='btn btn-secondary btn-lg'>
+            <button className='billboard_btn btn btn-secondary btn-lg'>
             <i className="icon info circle" />
               More Info
               </button>
-          </div>
-        </div>
-      </div>
+          </div>          
+        </div>     
+      </div>     
     </div>
   )
 }
