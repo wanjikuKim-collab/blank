2685a5e17182b8e8fa7dc5c126aaf34a086b14be
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-25 14:56:14 +0300

    billboard section structure

--- src/component/BillBoard/BillBoard.jsx
+++ src/component/BillBoard/BillBoard.jsx
@@ -8,9 +8,25 @@ function BillBoard() {
   return (
     <div className={styles.billboard}>
       <div className={styles.innerContainer}>
-        <img src={billboardHeroImage} alt="Arisu in the middle of Tokyo which has skyscrapers covered in overgrown plants" />
-        <div className={styles.info}>
-          <img src={billboardHeroTitle} alt="Alice in Boderland" />
+        {/* <div className={styles.imageWrapper}> */}
+          <img src={billboardHeroImage} alt="Arisu in the middle of Tokyo which has skyscrapers covered in overgrown plants" />
+        {/* </div> */}
+        <div className={styles.info} >
+          <img className={styles. heroImage} src={billboardHeroTitle} alt="Alice in Boderland" />
+          <div className={styles.description}>
+            An aimless gamer and his two friends find themselves in a paralle Tokyo, where they're forced to compete in a series of sadistic games to survive.
+          </div>
+          
+          <div className={styles. buttonLinks}>
+            <a className='btn btn-light btn-lg'> 
+            <i className="icon play" />
+              <span>Play</span>               
+            </a>
+            <button className='btn btn-secondary btn-lg'>
+            <i className="icon info circle" />
+              More Info
+              </button>
+          </div>
         </div>
       </div>
     </div>
