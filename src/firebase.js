fd7ee53187819a97eaaffa875d4d7b8129b3e812
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-27 02:53:50 +0300

    firebase set up and deployment

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDYN2cG8XYOiGObkekP68W-7NXEsYPHyFg",
+    authDomain: "netflix-clone-phase2-react.firebaseapp.com",
+    projectId: "netflix-clone-phase2-react",
+    storageBucket: "netflix-clone-phase2-react.appspot.com",
+    messagingSenderId: "1074850584963",
+    appId: "1:1074850584963:web:5b9f1fef116d784e773eb6"
+  };
