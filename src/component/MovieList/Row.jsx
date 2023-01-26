d8faab12e9ee2d0c1dc16d218ec6d6023f1ddb17
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-26 23:35:01 +0300

    scroll functionality

--- src/component/MovieList/Row.jsx
+++ src/component/MovieList/Row.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,useState } from 'react'
 import './Row.css'
 import axios from 'axios'
 
@@ -10,7 +10,6 @@ const bas_url = "https://image.tmdb.org/t/p/original"
 
 function Row({title,fetchURL, isLargeRow}) {
   const [movies, setMovies]=useState([]);
-
   //fetch request 
   useEffect(()=>{
     const fetchMovies = async()=>{
@@ -21,34 +20,50 @@ function Row({title,fetchURL, isLargeRow}) {
     fetchMovies()
   },[fetchURL])
 
+  //scroll event
+  function scrollLeft(){
+    let slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+
+  function scrollRight(){
+    let slider = document.getElementById('slider')
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
+
   console.log(movies)
 
   return (
     <div className='row'>
       <h2>{title}</h2>
-
-      <div className="row_posters">
-        {movies.map(movie=>{
-
-          /*to prevent dead links from coming in: it checks if its a large movie and the poster card exists
-          or if its not a large row and if the backdrop_pathis present 
-          and if the image is present display*/
-
-          return(
-            (isLargeRow && movie.poster_path)|| (!isLargeRow && movie.backdrop_path &&(
-              <img 
-              className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-              key={movie.id}
-              src={`${bas_url}${
-                isLargeRow? movie.poster_path : movie.backdrop_path
-              }`} 
-              alt={movie.name} />
-            ))
-        )
-        })}
+      <div className='slider_items'>
+        <div id="slider" className="row_posters">
+          <button onClick={scrollLeft} className='handlePrev'>
+            <i className="icon angle left"/>
+          </button>
+          {movies.map(
+            (movie)=>((isLargeRow && movie.poster_path)||(!isLargeRow && movie.backdrop_path))&&(
+                <img 
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                key={movie.id}
+                src={`${bas_url}${
+                  isLargeRow? movie.poster_path : movie.backdrop_path
+                }`} 
+                alt={movie.name} />
+              )
+              )}                
+          <button onClick={scrollRight} className='handleNext'>
+            <i className="icon angle right"/>
+          </button>
+        </div> 
       </div>
     </div>
   )
 }
 
 export default Row
+
+
+/*line 32: to prevent dead links from coming in: it checks if its a large movie and the poster card exists
+or if its not a large row and if the backdrop_pathis present 
+and if the image is present display*/
