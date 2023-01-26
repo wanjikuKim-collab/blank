8049bd191cddf6cdcc2cc475bbd737494151babd
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-01-27 00:34:30 +0300

    Navbar transition

--- src/component/NavBar/NavBar.jsx
+++ src/component/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SecondaryNAvigation from '../SecondaryNavigation/SecondaryNAvigation'
 import styles from "./NavBar.module.css"
 import { NavLink } from 'react-router-dom'
@@ -11,9 +11,20 @@ function NavBar() {
    { name:'Movies', link:"/movies"},
    { name:'My List', link:"/mylist"},
   ]
+// displaying black background on scrolling down
+  const [blackBG, setBlackBG] = useState(false)
+
+  function changeNavBackground(){
+    window.scrollY>100 ? setBlackBG(true) : setBlackBG(false);
+  }
+
+  useEffect(()=>{
+    window.addEventListener('scroll', changeNavBackground);
+    return ()=> window.removeEventListener('scroll',changeNavBackground)//cleanup
+  },[])
 
   return (
-    <div className={styles.nav}>
+    <div className={`${styles.nav} ${blackBG && `${styles.nav_black}`}`}>
       <div className={styles.mainNav}>
         <img className={styles.logo} src={logo} alt="Netflix Logo"/>      
         <ul className={styles.navItems}>
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -4,6 +4,7 @@ import Footer from "../component/Footer/Footer"
 import BillBoard from "../component/BillBoard/BillBoard";
 import Row from "../component/MovieList/Row";
 import requests from '../helpers/Request';
+import './HomeScreen.css'
 
 
 
@@ -12,13 +13,15 @@ function HomeScreen() {
     <div>
       <BillBoard />
       <NavBar/>
-      <Row title='Top Rated' isLargeRow fetchURL = {requests.fetchNowPlaying}/>
-      <Row title='Netflix Originals' fetchURL = {requests.fetchNetflixOriginals}/>
-      <Row title='Trending Now' fetchURL = {requests.fetchTrending}/>
-      <Row title='Comedy ' fetchURL = {requests.fetchComedy}/>   
-      <Row title='Action & Adventure' fetchURL = {requests.fetchAction}/>
-      <Row title='Documentaries' fetchURL = {requests.fetchDocumentaries}/> 
-      <Row title='Horror' fetchURL = {requests.fetchHorror}/>       
+      <div className="titles">
+        <Row title='Top Rated' isLargeRow fetchURL = {requests.fetchNowPlaying}/>
+        <Row title='Netflix Originals' fetchURL = {requests.fetchNetflixOriginals}/>
+        <Row title='Trending Now' fetchURL = {requests.fetchTrending}/>
+        <Row title='Comedy ' fetchURL = {requests.fetchComedy}/>   
+        <Row title='Action & Adventure' fetchURL = {requests.fetchAction}/>
+        <Row title='Documentaries' fetchURL = {requests.fetchDocumentaries}/> 
+        <Row title='Horror' fetchURL = {requests.fetchHorror}/>
+      </div>
       <Footer />
     </div>
   )
